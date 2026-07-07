f5dd448b6d06449eb80f786cfd2f75d2e74be533
张家瑞 <zhangjr0575@qq.com>
2026-07-07 16:01:03 +0800

    feat: 主题文件剥离到独立文件中

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,21 @@ import Layout from './layouts/index.vue';
 </script>
 
 <style lang="scss">
+@import url('@/assets/styles/theme.scss');
 @import url('@/assets/styles/animation.scss');
+
+:root {
+    color-scheme: light dark;
+    font-size: 14px;
+    min-height: 100vh;
+    color: var(--text);
+    background-color: var(--bg);
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, sans-serif;
+}
+
+body {
+  margin: 0;
+}
 
 .page {
   padding: 15px;
